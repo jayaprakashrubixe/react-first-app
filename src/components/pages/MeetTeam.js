17d75bb8071f11f87929f2d83fcb266ef2cd1e01
jayaprakash <jayaprakash.galla113@gmail.com>
2021-03-23 18:29:17 +0530

    message

--- src/components/pages/MeetTeam.js
+++ src/components/pages/MeetTeam.js
@@ -20,28 +20,28 @@ function MeetTeam() {
                         <div className="col-md-12">
                             <p>Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
                         </div>
-                        <div className="col-lg-3 mt-4">
+                        <div className="col-md-3 mt-4">
                             <div className="features-details meeteamdetails">
                                 <img src={Timg1} alt="image not display" />
                                 <h3 className="mt-2">Michel Logan</h3>
                                 <p>Founder</p>
                             </div>
                         </div>
-                        <div className="col-lg-3 mt-4">
+                        <div className="col-md-3 mt-4">
                             <div className="features-details meeteamdetails">
                                 <img src={Timg2} alt="image not display" />
                                 <h3 className="mt-2">David sean</h3>
                                 <p>Designer</p>
                             </div>
                         </div>
-                        <div className="col-lg-3 mt-4">
+                        <div className="col-md-3 mt-4">
                             <div className="features-details meeteamdetails">
                                 <img src={Timg3} alt="image not display" />
                                 <h3 className="mt-2">Alfred john</h3>
                                 <p>Developer</p>
                             </div>
                         </div>
-                        <div className="col-lg-3 mt-4">
+                        <div className="col-md-3 mt-4">
                             <div className="features-details meeteamdetails">
                                 <img src={Timg4} alt="image not display" />
                                 <h3 className="mt-2">Thomas lucas</h3>
