fd71c520cf83047be4c3d97cc76f985964324011
jayaprakash <jayaprakash.galla113@gmail.com>
2021-03-23 19:29:14 +0530

    sowmya added

--- src/components/pages/Features.js
+++ src/components/pages/Features.js
@@ -3,16 +3,16 @@ import FeatureSection from './FeatureSection';
 import HometopBanner from './HometopBanner';
 import MoreFeatures from './MoreFeatures';
 
-
+//fetaure js
 
 function Features() {
     const data = {
-        h2:'The best app for your data',
-        p:'Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
-      };
+        h2: 'The best app for your data',
+        p: 'Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip'
+    };
     return (
         <>
-            <HometopBanner data={data}/>
+            <HometopBanner data={data} />
             <FeatureSection />
             <MoreFeatures />
         </>
